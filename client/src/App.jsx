533bdcb8261d57b006cfd627ac52b469c624bc5c
soumen-github-account <soumendas7584@gmail.com>
2025-02-12 19:26:03 +0530

    Add missing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from './Pages/Home'
-import Login from './Pages/Login'
-import EmailVerify from './Pages/EmailVerify'
-import ResetPassword from './Pages/ResetPassword'
+import Home from './Pages/Home.jsx'
+import Login from './Pages/Login.jsx'
+import EmailVerify from './Pages/EmailVerify.jsx'
+import ResetPassword from './Pages/ResetPassword.jsx'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
