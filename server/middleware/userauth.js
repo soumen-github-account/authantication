43d1bfc33769ce59b25ee8dc3e4a159719e7a44a
soumen-github-account <soumendas7584@gmail.com>
2025-04-15 20:34:52 +0530

    hd1

--- server/middleware/userauth.js
+++ server/middleware/userauth.js
@@ -1,10 +1,25 @@
 import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next)=>{
-    const {token} =req.headers;
-    if(!token){
-        return res.json({ success: false, message: "not autharized login again" });
+    let token = req.headers.token || req.headers.authorization;
+
+    // Check for token in cookies
+    if (!token && req.cookies?.token) {
+        token = req.cookies.token;
     }
+
+    // Strip 'Bearer' if needed
+    if (token && token.startsWith("Bearer ")) {
+        token = token.split(" ")[1];
+    }
+
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Not authorized, please login again." });
+    }
+    // const {token} =req.headers;
+    // if(!token){
+    //     return res.json({ success: false, message: "not autharized login again" });
+    // }
     try{
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
 
