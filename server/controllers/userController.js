03480e496f7f6f7846dc9ff0033c55488b49305a
soumen-github-account <soumendas7584@gmail.com>
2025-04-15 21:22:29 +0530

    com

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import userModel from "../models/userModel.js";
 
 export const getUserData =async (req, res)=>{
     try{
-        const {userId} = req.body;
+        const userId = req.userId;
 
         const user = await userModel.findById(userId);
         if(!user){
