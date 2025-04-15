eda0dd134b1229a14629c47167ca4a48f8129928
soumen-github-account <soumendas7584@gmail.com>
2025-04-15 21:52:16 +0530

    df

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import userModel from "../models/userModel.js";
 
 export const getUserData =async (req, res)=>{
     try{
-        const userId = req.userId;
+        const {userId} = req.userId;
 
         const user = await userModel.findById(userId);
         if(!user){
--- server/middleware/userauth.js
+++ server/middleware/userauth.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next)=>{
-    const token = req.cookies.token;
+    // const token = req.cookies.token;
 
-    if (!token) {
-        return res.status(401).json({ success: false, message: "Token not found" });
-    }
-    // const {token} =req.headers;
-    // if(!token){
-    //     return res.json({ success: false, message: "not autharized login again" });
+    // if (!token) {
+    //     return res.status(401).json({ success: false, message: "Token not found" });
     // }
+    const {token} =req.headers;
+    if(!token){
+        return res.json({ success: false, message: "not autharized login again" });
+    }
     try{
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
 
