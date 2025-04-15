8697806157042e64cd8e63bd4db8f68a9f802690
soumen-github-account <soumendas7584@gmail.com>
2025-04-15 21:15:39 +0530

    sd1

--- server/server.js
+++ server/server.js
@@ -13,17 +13,17 @@ connectDB();
 const allowedOrigins = ['https://authantication-1.onrender.com','http://localhost:5173']
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: function(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  }));
-// app.use(cors({origin: allowedOrigins, credentials: true}))
+// app.use(cors({
+//     origin: function(origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true
+//   }));
+app.use(cors({origin: allowedOrigins, credentials: true}))
 // API endpoint
 app.get('/',(req,res)=>{
     res.send("api");
