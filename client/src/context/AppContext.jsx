d33cf94abac97ba74ddf6749758a47f84d6768f1
soumen-github-account <soumendas7584@gmail.com>
2025-04-15 20:50:18 +0530

    cre

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,7 +14,9 @@ export const AppContextProvider = (props)=>{
 
     const getAuthState = async ()=>{
         try {
-            const {data} = await axios.get(backendUrl + '/api/auth/is-auth')
+            const {data} = await axios.get(backendUrl + '/api/auth/is-auth', {
+                withCredentials: true,
+              })
             if(data.success){
                 setIsLoggedin(true)
                 getUserData()
